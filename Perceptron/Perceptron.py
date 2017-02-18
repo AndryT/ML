a9ec2fd9f10cbcc442fb6c16b76b6d94e2d625c0
AndryT <andrea.turchetto@gmail.com>
2017-02-18 19:34:51 +0000

    Added Adaline Batch Gradient Descent algorithm and started Adaline Stochastic Gradient Descent algorithm

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -77,6 +77,123 @@ class Perceptron(object):
         """ Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
         
+""" Adaptive Linear Neuron classifier (Adaline) - Batch Gradient Descent """
+
+class AdalineGD(object):
+    """ ADAptive LInear NEuron classifier 
+    
+    Paramters
+    -------------
+    eta: float
+        Learning rate (between 0.0 and 1.0).    
+    n_iter: int
+        Passes over the training dataset.
+    
+    Attributes
+    -------------
+    w_ : 1d-array
+        Weights after fitting.
+    cost_ : list
+        Number of missclassifications in every epoch.
+    
+    """
+    def __init__(self, eta = 0.1, n_iter = 50):
+        self.eta = eta
+        self.n_iter = n_iter
+            
+    
+    def fit(self, X, y):
+        """ Fit training data
+        
+        Parameters 
+        -------------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors,
+            where n_samples is the number of samples and n_feature is the 
+            number of features.
+        y : array-like, shape[n_samples]
+            Target values.
+        
+        Returns
+        ------------
+        self : object
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+        
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y-output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+        
+    def net_input(self, X):
+        """ Calculate Net-Input  """
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def activation(self, X):
+        """ Compute Linear Activation """
+        return self.net_input(X)
+    
+    def predict(self, X):
+        """ Return class label after unit step """
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+        
+""" Adaptive Linear Neuron classifier - Stochastic Gradient Descent """
+from numpy.random import seed
+class AdalineSGD(object):
+    """ ADAptive LInear NEuron classifier
+    
+    Parameters
+    -------------
+    eta : float
+        Learning rate (between 0.0 and 1.0).
+    n_iter : int
+        Passes over the training dataset.
+        
+    Attributes
+    -------------
+    w_ : 1d-array
+        Weights after fitting.
+    cost_ : list
+        Number of missclassifications in every epoch.
+    shuffle : bool (default: True)
+        Shuffles training data at every epoch if True to prevent cycles.
+    random_state : int (default : None)
+        Set random state for shuffling and initilizing the weights.
+
+    """        
+    def __init__(self, eta = 0.01, n_iter = 10, \
+                shuffle = True, random_state = None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+    
+    def fit(self, X, y):
+        """ Fit training data.
+        
+        Parameters
+        -------------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and
+            n_features is the number of features,
+        y : array-like, shape = [n_samples]
+            Target values
+        
+        Returns
+        ------------
+        self : object
+        
+        """
+            
+        
+        
 """ 
     1) Import Iris dataset into a DataFrame using Pandas
     2) Check the last 5 lines of the import dataset
@@ -155,4 +272,41 @@ plot_decision_regions(X, y, classifier = ppn)
 plt.xlabel('Sepal length [cm]')
 plt.ylabel('Petal length [cm]')
 plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+
+""" Testing the Adaline algorithm """
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (8,4))
+ada1 = AdalineGD(eta = 0.01, n_iter = 10).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_)+1), np.log10(ada1.cost_), marker = 'o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+ada2 = AdalineGD(eta = 0.0001, n_iter = 10).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_)+1), np.log10(ada2.cost_), marker = 'o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(Sum-squared-error)')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
+plt.close()
+
+""" Standardization of Data """
+X_std = np.copy(X)
+for i in range(X_std.shape[1]):
+    X_std[:,i] = (X[:,i]-X[:,i].mean())/X[:,i].std()
+#X_std[:,0] = (X[:,0]-X[:,0].mean())/X[:,0].std()
+#X_std[:,1] = (X[:,1]-X[:,1].mean())/X[:,1].std()
+ada = AdalineGD(eta = 0.01, n_iter = 15)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier = ada)
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('Petal length [standardized]')
+plt.title('Adaline - Gradient Descent')
+plt.legend(loc = 'upper left')
+plt.show()
+plt.figure()
+plt.plot(range(1, len(ada.cost_) +1), ada.cost_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
 plt.show()
