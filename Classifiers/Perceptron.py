63b05a71629e5080cd4d1cda2046350ec094aadf
AndryT <andrea.turchetto@gmail.com>
2017-02-19 15:56:20 +0000

    Implemented Adaline Stochastic Gradient Descent and change main folder name from Perceptron to Classifier

--- Classifiers/Perceptron.py
+++ Classifiers/Perceptron.py
@@ -144,6 +144,7 @@ class AdalineGD(object):
         
 """ Adaptive Linear Neuron classifier - Stochastic Gradient Descent """
 from numpy.random import seed
+
 class AdalineSGD(object):
     """ ADAptive LInear NEuron classifier
     
@@ -190,9 +191,62 @@ class AdalineSGD(object):
         ------------
         self : object
         
-        """
-            
+        """              
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
         
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+        
+    def partial_fit(self, X, y):
+        """ Fit training data without reinitializing the weights """
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+        
+    def _shuffle(self, X, y):
+        """ Shuffle training data """
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+    
+    def _initialize_weights(self, m):
+        """ Initialize weights to zeros """
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+        
+    def _update_weights(self, xi, target):
+        """ Apply Adaline learning rule to update the weights """
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+        
+    def net_input(self, X):
+        """ Calculate net input """
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+        
+    def activation(self, X):
+        """ Compute linear activation """
+        return self.net_input(X)
+        
+    def predict(self, X):
+        """ Return class label after unit step """
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
         
 """ 
     1) Import Iris dataset into a DataFrame using Pandas
@@ -297,6 +351,8 @@ for i in range(X_std.shape[1]):
     X_std[:,i] = (X[:,i]-X[:,i].mean())/X[:,i].std()
 #X_std[:,0] = (X[:,0]-X[:,0].mean())/X[:,0].std()
 #X_std[:,1] = (X[:,1]-X[:,1].mean())/X[:,1].std()
+    
+""" Testing AdalineGD classifier """
 ada = AdalineGD(eta = 0.01, n_iter = 15)
 ada.fit(X_std, y)
 plot_decision_regions(X_std, y, classifier = ada)
@@ -305,8 +361,26 @@ plt.ylabel('Petal length [standardized]')
 plt.title('Adaline - Gradient Descent')
 plt.legend(loc = 'upper left')
 plt.show()
+plt.close()
 plt.figure()
 plt.plot(range(1, len(ada.cost_) +1), ada.cost_, marker = 'o')
 plt.xlabel('Epochs')
 plt.ylabel('Sum-squared-error')
+plt.show()
+plt.close()
+
+
+""" Testing AdalineSGD classifier """
+ada = AdalineSGD(eta = 0.01, n_iter = 15, random_state = 1)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier = ada)
+plt.title('Adaline - Stichastic Gradient Descent')
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('Petal length [standardized]')
+plt.legend(loc = 'upper left')
+plt.show()
+plt.figure()
+plt.plot(range(1, len(ada.cost_)+1), ada.cost_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('Avarage cost')
 plt.show()
