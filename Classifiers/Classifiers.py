c64319330d154baaaf3962b0eca1a2aadd38c161
AndryT <andrea.turchetto@gmail.com>
2017-02-21 15:51:59 +0000

    Added Random Forest model and K-nearest neighbors

--- Classifiers/Classifiers.py
+++ Classifiers/Classifiers.py
@@ -62,6 +62,7 @@ def plot_decision_regions(X, y, classifier, test_idx = None, resolution = 0.02):
     markers = ['s','x','o','^','v']
     colors = ['red','blue','lightgreen','gray','cyan']
     cmap = ListedColormap(colors[:len(np.unique(y))])
+  
     
     # Plot the decision surface - for 2 Features only !!!
     x1_min, x1_max = X[:,0].min()-1, X[:,0].max()+1
@@ -170,4 +171,124 @@ plot_decision_regions(X_combined_std, y_combined, classifier = svm,\
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc = 'upper left')
+plt.show()
+plt.close()
+
+""" Using linear classifier for large amount of data 
+    => Stochastic Gradient Descent algorithm
+"""
+from sklearn.linear_model import SGDClassifier
+ppn = SGDClassifier(loss = 'perceptron') # perceptron
+lr = SGDClassifier(loss = 'log') # logistic regression
+svm = SGDClassifier(loss = 'hinge') # Support Vector Machine
+
+""" Solving non-linear problems using Kernel SVM """
+# Create a simple dataset
+np.random.seed(0)
+X_xor = np.random.randn(200, 2) # sample from standard normal distribution
+y_xor = np.logical_xor(X_xor[:,0] > 0, X_xor[:,1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+plt.scatter(X_xor[y_xor == 1, 0], X_xor[y_xor == 1, 1], \
+        c = 'b', marker = 'x', label = '1')
+plt.scatter(X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1], \
+        c = 'r', marker = 's', label = '-1')
+plt.ylim(-3.0)
+plt.legend()
+plt.show()
+plt.close()
+# using the Kernel to identify the decision boundaries - Radial Basis Function RBF
+#from sklearn.svm import SVC
+svm = SVC(kernel = 'rbf', C = 10.0, gamma = 0.1, random_state = 0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier = svm)
+plt.legend(loc = 'upper left')
+plt.show()
+plt.close()
+
+""" Test the Kernel function to the Iris dataset 
+    Check sensitivity of gamma parameter
+"""
+# gamma = 0.2 --> Small gamma = soft boundaries
+gamma_values = [0.1, 1, 10, 100]
+for g in gamma_values:
+    svm = SVC(kernel = 'rbf', random_state = 0, C = 10.0, gamma = g)
+    svm.fit(X_combined_std, y_combined)
+    plt.figure()    
+    plot_decision_regions(X_combined_std, y_combined, classifier = svm, \
+                          test_idx = range(len(X_train_std),len(X_combined_std)))
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc = 'upper left')
+    plt.show()
+plt.close('all')
+
+""" Decision Tree - Impurity models for 1 class """
+# Gini impurity:
+def gini(p):
+    return (p)*(1-p)+(1-p)*(1-(1-p))
+# Entropy:
+def entropy(p):
+    return -(p)*np.log2(p)-(1-p)*np.log2(1-p)
+# Missclassification Error:
+def error(p):
+    return 1-np.max([p, 1-p])
+x = np.arange(0.0, 1.0, 0.01)
+ent = [entropy(p) if p!=0 else None for p in x]
+sc_ent = [0.5*e if e else None for e in ent]
+err = [error(i) for i in x]
+fig = plt.figure()
+ax = plt.subplot(111)
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err], \
+    ['Entropy', 'Entropy (scaled)', 'Gini Impurity', 'Missclassification Error'],\
+    ['-','-','--','-.'], ['black','lightgray','red','green','cyan']):
+        line = ax.plot(x, i, label=lab, linestyle = ls, lw=2, color = c)
+ax.legend(loc='upper center', bbox_to_anchor = (0.5, 1.15), \
+    ncol = 3, fancybox = True, shadow=False)
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+plt.show()
+
+""" Building a Decision Tree """
+from sklearn.tree import DecisionTreeClassifier
+tree = DecisionTreeClassifier(criterion = 'entropy', max_depth = 3,\
+                random_state = 0)
+tree.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+plot_decision_regions(X_combined, y_combined, classifier = tree,\
+        test_idx = range(len(X_train),len(X_combined)))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+# Export the decision tree to graphviz:
+from sklearn.tree import export_graphviz
+export_graphviz(tree, out_file = 'tree.dot', \
+    feature_names = ['petal length', 'petal width'])
+    
+""" Random Forest to ensemble different tree classifiers """
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(criterion = 'entropy', n_estimators = 10,\
+        random_state = 1, n_jobs = 2)
+forest.fit(X_train, y_train)
+plot_decision_regions(X_combined, y_combined, classifier = forest,\
+                      test_idx = range(len(X_train),len(X_combined)))
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.show()
+plt.close()
+
+""" K-Nearest Neighbors classifier """
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 5, p =2, metric = 'minkowski')
+knn.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier = knn, \
+    test_idx = range(len(X_train_std), len(X_combined_std)))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
 plt.show()
