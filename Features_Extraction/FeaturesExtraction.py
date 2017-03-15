a2f8848f3f125db7c656efe0e72001d17fc41907
AndryT <andrea.turchetto@gmail.com>
2017-03-15 18:25:57 +0000

    Completed LDA algorithm in FeatureExtraction file

--- Features_Extraction/FeaturesExtraction.py
+++ Features_Extraction/FeaturesExtraction.py
@@ -122,6 +122,12 @@ X_train_pca = pca.fit_transform(X_train_std)
 pca.explained_variance_ratio_
 
 """ Linear Discriminant Analysis (LDA) - Supervised data compression """
+# Load database and prepare training and test data
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:,0].values
+X_train, X_test, y_train, y_test = train_test_split(X , y, test_size = 0.3, random_state = 0)
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
 # Calculate mean vectors
 np.set_printoptions(precision=4)
 mean_vecs = []
@@ -138,3 +144,77 @@ for label, mv in zip(np.unique(y_test), mean_vecs):
         class_scatter += (row-mv).dot((row-mv).T)
     S_W += class_scatter
 print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+# Check wheteher the labels in the training set are uniformly disgtributed
+print("Class label distribution: %s" % np.bincount(y_train)[1:])
+# Normalize the Within-class scatter matrix
+# d = 13 number of features
+S_W = np.zeros((d,d))
+for label, mv in zip(np.unique(y_test), mean_vecs):
+    class_scatter = np.cov(X_train_std[y_train == label].T)
+    S_W += class_scatter
+print('Scaled within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
+# Compute between-class scatter matrix
+mean_overall = np.mean(X_train_std, axis=0)
+mean_overall = mean_overall.reshape(d, 1)
+# d = 13 number of features
+S_B = np.zeros((d,d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train[y_train==i+1, :].shape[0]
+    mean_vec = mean_vec.reshape(d, 1)
+#    mean_overall = mean_overall.reshape(d, 1)
+    S_B += n*(mean_vec - mean_overall).dot((mean_vec-mean_overall).T)
+print('Between-class scatter matrix: %sx%s' % (S_B.shape[0], S_B.shape[1]))
+""" Slelecting the linear discriminants for the new feature subspace """
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in \
+    range(len(eigen_vals))]
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True) # k[0] = eigen_value
+print('Eigenvalues in decreasing order:\n')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+# Class-discriminatory information (discriminability)
+tot = sum(eigen_vals.real)
+discr = [(i/tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr  = np.cumsum(discr)
+plt.bar(range(1,14), discr, alpha = 0.5, align = 'center',\
+     label='individual "discriminability"')
+plt.step(range(1,14), cum_discr, where = 'mid', \
+    label = 'cumulative "discriminability"')
+plt.ylabel('"Discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc = 'best')
+plt.show()
+# Create the tramsformation matrix W using the first 2 most discriminative eigenvectors columns
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real, \
+                eigen_pairs[1][1][:, np.newaxis].real))
+print('Matrix W:\n', w)
+# Projecting samples onto the new feature space
+X_train_lda = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l,c,m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_lda[y_train==l, 0]*(-1), X_train_lda[y_train==l, 1]*(-1),\
+        color=c, label=l, marker=m)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower right')
+plt.show()
+""" LDA using the scikit learning tools """
+from sklearn.lda import LDA
+lda = LDA(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+plot_decision_regions(X_train_lda, y_train, classifier = lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc = 'lower left')
+plt.show()
+# Plot the test set
+X_test_lda = lda.transform(X_test_std)
+plot_decision_regions(X_test_lda, y_test, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc = 'lower left')
+plt.show()
