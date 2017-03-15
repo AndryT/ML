dbeb2c26245043531b83b283c238bb654dab9c7f
AndryT <andrea.turchetto@gmail.com>
2017-03-15 23:41:38 +0000

    Implemented Kernel PCA algorithm using Radia Basis Function. Implementation and completion of FeatureExtraction

--- Features_Extraction/FeaturesExtraction.py
+++ Features_Extraction/FeaturesExtraction.py
@@ -218,3 +218,194 @@ plt.xlabel('LD 1')
 plt.ylabel('LD 2')
 plt.legend(loc = 'lower left')
 plt.show()
+
+""" Kernel Principal Component Analysis (Kernel-PCA) - Radial Basis Function (RBF)"""
+from scipy.spatial.distance import pdist, squareform
+from scipy import exp
+from scipy.linalg import eigh
+#import numpy as np
+
+def rbf_kernel_pca(X, gamma, n_components):
+    """
+    RBF Keernel PCA implementation.
+    
+    Parameters
+    --------------
+    X: {NumPy ndarray}, shape = [n_samples, n_features]
+    
+    gamma: float
+        Tuning parameter of the RBF kernel
+    
+    n_components: int
+        Number of principal components to return
+     
+    Returns
+    -------------
+    X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
+        Projected dataset
+    """
+    # Calculate pairwise squared Euclidian distances in the MxN dimensional dataset
+    sq_dists = pdist(X, 'sqeuclidean')
+        
+    # Convert pairwise distances into a square matrix
+    mat_sq_dists = squareform(sq_dists)
+    
+    # Compute the symmetric kernel matrix
+    K = exp(-gamma * mat_sq_dists)
+    
+    # Center the kernel matrix
+    N = K.shape[0]
+    one_n = np.ones((N,N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+    
+    # Obtaining eigenpairs from the centered kernel matrix
+    # numpy.eigh return them in sorted order
+    eigvals, eigvecs = eigh(K)
+    
+    # Collect the top k eigenvectors (projected samples)
+    X_pc = np.column_stack((eigvecs[:,-i] for i in range(1, n_components+1)))
+    
+    return X_pc
+
+""" Separating half-moon shapes """
+from sklearn.datasets import make_moons
+X, y = make_moons(n_samples = 100, random_state = 123)
+plt.scatter(X[y==0, 0], X[y==0, 1], color='r', marker='^', alpha=0.5)
+plt.scatter(X[y==1, 0], X[y==1, 1], color='b', marker='o', alpha=0.5)
+plt.show()
+
+# from sklearn.decomposition import PCA
+scikit_pca = PCA(n_components = 2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_spca[y==0,0], X_spca[y==0,1], color='r', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1,0], X_spca[y==1,1], color='b', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y==0,0], np.zeros((50,1))+0.02,\
+                color='r', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1,0], np.zeros((50,1))-0.02,\
+                color='b', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC 1')
+ax[0].set_ylabel('PC 2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC 1')
+plt.show()
+# Use Kernel-PCA (RBF)
+from matplotlib.ticker import FormatStrFormatter
+X_kpca = rbf_kernel_pca(X, gamma = 15, n_components = 2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y==0,0], X_kpca[y==0,1], color='r', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y==1,0], X_kpca[y==1,1], color='b', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y==0,0], np.zeros((50,1))+0.02, \
+    color='r', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1,0], np.zeros((50,1))-0.02, \
+    color='b', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC 1')
+ax[0].set_ylabel('PC 2')
+ax[1].set_xlabel('PC 1')
+ax[1].set_yticks([])
+ax[1].set_ylim([-1, 1])
+ax[1].set_xlabel('PC 1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+plt.show()
+
+""" Separating concentric circles """
+from sklearn.datasets import make_circles
+X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
+plt.scatter(X[y==0, 0], X[y==0, 1], color='r', marker='^', alpha=0.5)
+plt.scatter(X[y==1, 0], X[y==1, 1], color='b', marker='o', alpha=0.5)
+plt.show()
+
+scikit_pca = PCA(n_components = 2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_spca[y==0,0],X_spca[y==0,1], color='r', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1,0],X_spca[y==1,1], color='b', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y==0,0], np.zeros((500,1))+0.02, \
+    color='r', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1,0], np.zeros((500,1))-0.02, \
+    color='b', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC 1')
+ax[0].set_ylabel('PC 2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC 1')
+plt.show()
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components = 2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3)) 
+ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1], \
+    color='r', marker='^', alpha = 0.5)
+ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1], \
+    color='b', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((500,1))+0.02, \
+    color='r', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((500,1))-0.02, \
+    color='b', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC 1')
+ax[0].set_ylabel('PC 2')
+ax[1].set_xlabel('PC 1')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+plt.show()
+
+""" Projecting New data Point """
+# Modify the RBF Kernel PCA algorithm in order tp return both eigenvectors and
+# its eigenvalues. This is done to be able to normalize the eigenvectors (alpha)
+# by its eigenvalues (lambda)
+
+def rbf_kernel_pca(X, gamma, n_components):
+    """ Similar to the previous method """
+    sq_dists = pdist(X, 'sqeuclidean')
+    mat_sq_dists = squareform(sq_dists)
+    K = exp(-gamma * mat_sq_dists)
+    N = K.shape[0]
+    one_n = np.ones((N,N))/N
+    K = K-one_n.dot(K)-K.dot(one_n)+one_n.dot(K).dot(one_n)
+    # Sorted eigenvectors and eigenvalues   
+    eigvals, eigvecs = eigh(K)
+    # Eigenvectors
+    alphas = np.column_stack((eigvecs[:,-i] for i in range(1, n_components+1)))
+    # Eigenvalues    
+    lambdas = [eigvals[-i] for i in range(1, n_components+1)]
+    
+    return alphas, lambdas
+    
+# Testing modified rbf_kernel_pca algorithm
+X, y = make_moons(n_samples = 100, random_state=123)
+alphas, lambdas = rbf_kernel_pca(X, gamma = 15, n_components=1)
+x_new = X[25]
+print(x_new)
+x_proj = alphas[25] # original projection
+print(x_proj)
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new-row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas/lambdas)
+
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+x_reproj
+
+plt.scatter(alphas[y==0, 0], np.zeros((50)), color='r', marker='^', alpha=0.5)
+plt.scatter(alphas[y==1, 0], np.zeros((50)), color='b', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='k', label='original projection of point X[25]',\
+    marker='^', s=100)
+plt.scatter(x_reproj, 0, color='g', label='remapped point X[25]', \
+    marker='x', s=500)
+plt.legend(scatterpoints=1)
+plt.show()
+
+""" Kernel PCA in scikit learning """
+from sklearn.decomposition import KernelPCA
+X, y = make_moons(n_samples = 100, random_state=123)
+scikit_kpca = KernelPCA(n_components = 2, kernel = 'rbf', gamma = 15)
+X_skernpca = scikit_kpca.fit_transform(X)
+plt.scatter(X_skernpca[y==0, 0], X_skernpca[y==0, 1], \
+    color='r', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y==1, 0], X_skernpca[y==1, 1], \
+    color='b', marker = 'o', alpha = 0.5)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.show()
